a88de4654d79cd24abddb0e8b5af3510da0ecc23
Dominik Furman <domfurman01@gmail.com>
2024-12-08 20:14:34 +0100

    create components for weather details and weekly summary

---
+++ src/app/weekly-summary/weekly-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weekly-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './weekly-summary.component.html',
+  styleUrl: './weekly-summary.component.scss'
+})
+export class WeeklySummaryComponent {
+
+}
